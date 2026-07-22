f482ff5f344cdcbcf7ea22e9c3098afe710ad301
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-07-22 09:55:53 +0530

    Update 10. Line Plot and Bar Plot using Matplotlib.py

--- 10. Line Plot and Bar Plot using Matplotlib.py
+++ 10. Line Plot and Bar Plot using Matplotlib.py
@@ -1,5 +1,12 @@
-import matplotlib.pyplot as plt
+10. Scenario: You are working on a data visualization project and need to create basic plots using
+Matplotlib. You have a dataset containing the monthly sales data for a company, including the month
+and corresponding sales values. Your task is to develop a Python program that generates line plots and
+bar plots to visualize the sales data.
+Question:
+1. How would you develop a Python program to create a line plot of the monthly sales data?
+2: How would you develop a Python program to create a bar plot of the monthly sales data?
 
+import matplotlib.pyplot as plt
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
 sales = [2000, 2500, 3000, 2800, 3500, 4000]
 
