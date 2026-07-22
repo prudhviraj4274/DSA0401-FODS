101c615eff8439709bbdf75b15654e9165dcaa61
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-07-22 09:51:53 +0530

    Update 2.Sales.py

--- 2.Sales.py
+++ 2.Sales.py
@@ -1,3 +1,8 @@
+2. Scenario: You are a data analyst working for a company that sells products online. You have been
+tasked with analyzing the sales data for the past month. The data is stored in a NumPy array.
+Question: How would you find the average price of all the products sold in the past month? Assume
+3x3 matrix with each row representing the sales for a different product
+
 import numpy as np
 sales = np.array([
     [100, 120, 110],
