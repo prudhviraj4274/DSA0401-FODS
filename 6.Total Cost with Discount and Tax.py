7da5c080321708fef6b1af18c372124429982587
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-07-22 09:53:35 +0530

    Update 6.Total Cost with Discount and Tax.py

--- 6.Total Cost with Discount and Tax.py
+++ 6.Total Cost with Discount and Tax.py
@@ -1,3 +1,10 @@
+6. Scenario: You are a cashier at a grocery store and need to calculate the total cost of a customer's
+purchase, including applicable discounts and taxes. You have the item prices and quantities in separate
+lists, and the discount and tax rates are given as percentages. Your task is to calculate the total cost for
+the customer.
+Question: Use arithmetic operations to calculate the total cost of a customer's purchase, including
+discounts and taxes, given the item prices, quantities, discount rate, and tax rate?
+                                                                                                        
 import numpy as np
 prices = np.array([100, 200, 50, 80])
 quantities = np.array([2, 1, 4, 3])
