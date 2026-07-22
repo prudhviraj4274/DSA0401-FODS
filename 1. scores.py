c6fd5d4af81acad79639769773a5a97f148d17a1
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-07-22 09:51:26 +0530

    Update 1. scores.py

--- 1. scores.py
+++ 1. scores.py
@@ -1,3 +1,12 @@
+1. Scenario: You are working on a project that involves analyzing student performance data for a
+class of 32 students. The data is stored in a NumPy array named student_scores, where each row
+represents a student and each column represents a different subject. The subjects are arranged in the
+following order: Math, Science, English, and History. Your task is to calculate the average score for
+each subject and identify the subject with the highest average score.
+Question: How would you use NumPy arrays to calculate the average score for each subject and
+determine the subject with the highest average score? Assume 4x4 matrix that stores marks of each
+student in given order.
+    
 import numpy as np
 student_scores = np.array([
     [85, 90, 78, 88],
