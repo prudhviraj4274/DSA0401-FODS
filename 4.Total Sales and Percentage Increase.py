9e4c16a10cbcb957a62ff1f6e38c1c96462e5a57
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-07-22 09:52:47 +0530

    Update 4.Total Sales and Percentage Increase.py

--- 4.Total Sales and Percentage Increase.py
+++ 4.Total Sales and Percentage Increase.py
@@ -1,3 +1,11 @@
+4. Scenario: You are working on a project that involves analyzing the sales performance of a company
+over the past four quarters. The quarterly sales data is stored in a NumPy array named sales_data,
+where each element represents the sales amount for a specific quarter. Your task is to calculate the
+total sales for the year and determine the percentage increase in sales from the first quarter to the fourth
+quarter.
+Question: Using NumPy arrays and arithmetic operations calculate the total sales for the year and
+determine the percentage increase in sales from the first quarter to the fourth quarter?
+  
 import numpy as np
 sales_data = np.array([50000, 60000, 70000, 80000])
 total_sales = np.sum(sales_data)
