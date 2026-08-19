78f5b44c586cf6528b236d8007ef8536f42a784c
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-08-19 11:25:33 +0530

    Update 11. Sales Data Visualization using Matplotlib.py

--- 11. Sales Data Visualization using Matplotlib.py
+++ 11. Sales Data Visualization using Matplotlib.py
@@ -1,3 +1,12 @@
+11. Scenario : You are a data scientist working for a company that sells products online. You have
+been tasked with creating a simple plot to show the sales of a product over time.
+
+Question:
+1. Write code to create a simple line plot in Python using Matplotlib to predict sales happened in a
+month?
+2. Write code to create a scatter plot in Python using Matplotlib to predict sales happened in a month?
+3. Develop a Python program to create a bar plot of the monthly sales data.
+
 import matplotlib.pyplot as plt
 
 months = ["Jan","Feb","Mar","Apr","May","Jun"]
