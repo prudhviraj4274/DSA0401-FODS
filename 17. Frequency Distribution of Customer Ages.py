a1095923dd1b9cde3e7dbf499e5ec111c033f8a1
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-08-19 11:39:51 +0530

    Update 17. Frequency Distribution of Customer Ages.py

--- 17. Frequency Distribution of Customer Ages.py
+++ 17. Frequency Distribution of Customer Ages.py
@@ -1,3 +1,8 @@
+17. Scenario: You are a data analyst working for a company that sells products online. You have been
+tasked with analyzing the sales data for the past month. The data is stored in a Pandas data frame.
+Question: Develop a code in python to find the frequency distribution of the ages of the customers who
+have made a purchase in the past month.
+    
 import pandas as pd
 
 sales_data = pd.DataFrame({
