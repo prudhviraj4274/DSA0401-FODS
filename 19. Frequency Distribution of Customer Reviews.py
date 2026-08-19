af1dda519d8e22c74a6a722eff3d355d0522c4c0
B.PRUDHVI RAJ <prudhvirajb4274.sse@saveetha.com>
2026-08-19 11:40:40 +0530

    Update 19. Frequency Distribution of Customer Reviews.py

--- 19. Frequency Distribution of Customer Reviews.py
+++ 19. Frequency Distribution of Customer Reviews.py
@@ -1,3 +1,9 @@
+19. Scenario: You are working on a project that involves analyzing customer reviews for a product.
+You have a dataset containing customer reviews, and your task is to develop a Python program that
+calculates the frequency distribution of words in the reviews.
+Question: Develop a Python program to calculate the frequency distribution of words in the customer
+reviews dataset?
+
 from collections import Counter
 
 reviews = """
